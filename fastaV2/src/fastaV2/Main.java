d13b59ca6655bc86cfbb9d76fa30c7fa8cd0c62c
rumangerst <ruman.gerst@gmx.de>
2014-05-27 10:13:46 +0200

    * Initial commit

---
+++ fastaV2/src/fastaV2/Main.java
@@ -0,0 +1,29 @@
+package fastaV2;
+
+public class Main
+{
+
+	public static void main(String[] args)
+	{
+		Fasta fasta1 = new Fasta(">Sequenz 1", "CCATGCTGCCACAGGGCTTCATGTTGATGCCGGTAGTCTGTGCTCCATCT"); //alternative: CCATGCTGCCACAGGGCTTCATGTTGATGCCGGTAGTCTGTGCTCCATCT
+		Fasta fasta2 = new Fasta(">Homo sapiens (human) pyruvate: carbon-dioxide ligase", "ATGCTGAAGTTCCGAACAGTCCATGGGGGCCTGAGGCTCCTGGGAATCCGCCGAACCTCC");
+		
+		fasta1.print();
+		System.out.println();
+		fasta2.print();
+		
+		System.out.println();
+		System.out.println();
+		System.out.println("-- Dotplot von einer Sequenz zu sich selbst:");
+		
+		//dotplot (self)
+		fasta1.dotPlotAgainst(fasta1);
+		
+		System.out.println();		
+		System.out.println("-- Dotplot beider Sequenzen:");
+		
+		//dotplot (both)
+		fasta1.dotPlotAgainst(fasta2); 
+	}
+
+}
